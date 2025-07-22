8bf42d28de630524f46d1889b394263f41363120
KATERYNA <kvargatiuk@gmail.com>
2025-07-22 13:24:27 +0200

    task5 (oop)

---
+++ src/main/java/oop/homwork/task5/Fish.java
@@ -0,0 +1,8 @@
+package oop.homwork.task5;
+
+public class Fish extends Animal{
+    @Override
+    public void move(){
+        System.out.println("Риба пливе");
+    }
+}
---
+++ src/main/java/oop/homwork/task5/test5.java
@@ -0,0 +1,13 @@
+package oop.homwork.task5;
+
+public class test5 {
+    public static void main(String[] args) {
+        Animal animal = new Bird();
+        Animal animal1=new Fish();
+        Animal animal2= new Dog();
+
+        animal.move();
+        animal1.move();
+        animal2.move();
+    }
+}
