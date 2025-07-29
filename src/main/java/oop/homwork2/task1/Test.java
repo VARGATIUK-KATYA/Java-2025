3b8b99fd28de4c428b750a823099a2735a5ca9f0
KATERYNA <kvargatiuk@gmail.com>
2025-07-29 11:33:57 +0200

    task1 (this)

---
+++ src/main/java/oop/homwork2/task1/Test.java
@@ -0,0 +1,10 @@
+package oop.homwork2.task1;
+
+public class Test {
+    public static void main(String[] args) {
+        Car car = new Car();
+        car.setBrand("Tayota");
+        System.out.println(car.getBrand());
+
+    }
+}
