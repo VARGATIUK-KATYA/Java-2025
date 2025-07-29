b6c507ba94542a90c9166ee0f7c1ba8db31653ab
KATERYNA <kvargatiuk@gmail.com>
2025-07-29 11:37:54 +0200

    task3 (object)

---
+++ src/main/java/oop/homwork2/task3/Test.java
@@ -0,0 +1,12 @@
+package oop.homwork2.task3;
+
+public class Test {
+    public static void main(String[] args) {
+        User user=new User();
+        user.setName("Іван");
+        user.getThis().display();
+
+    }
+
+
+}
