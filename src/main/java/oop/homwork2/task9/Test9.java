29999773f462c8390581235086111c983ad96f1e
KATERYNA <kvargatiuk@gmail.com>
2025-07-29 12:36:23 +0200

    task9 (finalpolya)

---
+++ src/main/java/oop/homwork2/task9/Test9.java
@@ -0,0 +1,8 @@
+package oop.homwork2.task9;
+
+public class Test9 {
+    public static void main(String[] args) {
+        ImmutablePoint immutablePoint=new ImmutablePoint(5, 10);
+        immutablePoint.print();
+    }
+}
